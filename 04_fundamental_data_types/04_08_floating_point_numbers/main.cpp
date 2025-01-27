9d8767bdaaee8db9a590e9f74ba260c4bfa178bd
RicLourenco <ric.filip85@gmail.com>
2025-01-27 16:49:40 +0000

    Outputting floating point values

--- 04_fundamental_data_types/04_08_floating_point_numbers/main.cpp
+++ 04_fundamental_data_types/04_08_floating_point_numbers/main.cpp
@@ -68,10 +68,96 @@ A floating point type can only precisely represent a certain number of significa
 digits. Using a value with more significant digits than the minimum may result in
 the value being stored inexactly. */
 
+
+
+/* Outputting floating point values
+
+When outputting floating point numbers, std::cout has a default precision of 6 --
+that is, it assumes all floating point variables are only significant to 6 digits
+(the minimum precision of a float), and hence it will truncate anything after that
+
+The following program shows std::cout truncating to 6 digits: */
+
+void ex3()
+{
+    std::cout << 9.87654321f << '\n';
+    std::cout << 987.654321f << '\n';
+    std::cout << 987654.321f << '\n';
+    std::cout << 9876543.21f << '\n';
+    std::cout << 0.0000987654321f << '\n';
+}
+
+/* This program outputs:
+9.87654
+987.654
+987654
+9.87654e+006
+9.87654e-005
+
+9.87654e+006 is the same as 9.87654e6, just with some padding 0’s (depends on
+compiler used)
+
+We can override the default precision that std::cout shows by using an output
+manipulator function named std::setprecision(). Output manipulators alter how data
+is output, and are defined in the iomanip header */
+
+#include <iomanip>
+
+void ex4()
+{
+    std::cout << std::setprecision(17); 
+    std::cout << 3.33333333333333333333333333333333333333f <<'\n'; 
+    std::cout << 3.33333333333333333333333333333333333333 << '\n';
+}
+
+/* Outputs:
+3.3333332538604736
+3.3333333333333335
+
+Because we set the precision to 17 digits using std::setprecision(), each of the
+above numbers is printed with 17 digits. But, as you can see, the numbers certainly
+aren’t precise to 17 digits! And because floats are less precise than doubles, the
+float has more error.
+
+Tip
+Output manipulators (and input manipulators) are sticky -- meaning if you set them,
+they will remain set.
+The one exception is std::setw. Some IO operations reset std::setw, so std::setw
+should be used every time it is needed.
+
+Precision issues don’t just impact fractional numbers, they impact any number with
+too many significant digits. Let’s consider a big number: */
+
+void ex5()
+{
+    float f { 123456789.0f };
+    std::cout << std::setprecision(9);
+    std::cout << f << '\n';
+}
+
+/* Output:
+
+123456792
+
+123456792 is greater than 123456789. The value 123456789.0 has 10 significant
+digits, but float values typically have 7 digits of precision (and the result of
+123456792 is precise only to 7 significant digits). We lost some precision! When
+precision is lost because a number can’t be stored precisely, this is called a
+rounding error.
+Consequently, one has to be careful when using floating point numbers that require
+more precision than the variables can hold.
+
+Best practice
+Favor double over float unless space is at a premium, as the lack of precision in a
+float will often lead to inaccuracies. */
+
 int main()
 {
     ex1();
     ex2();
+    ex3();
+    ex4();
+    ex5();
 
     return 0;
 }
